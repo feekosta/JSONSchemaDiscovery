23646bbcc1f9c03ee020d2b77dd1e6bf3c068ac0
Felipe Costa <feekosta@gmail.com>
2017-10-04 23:58:17 -0300

    chore: download jsonschema result

--- client/app/_components/batch/batch.component.ts
+++ client/app/_components/batch/batch.component.ts
@@ -1,3 +1,4 @@
+import * as FileSaver from 'file-saver';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { JsonSchemaService } from '../../_services/services';
@@ -20,6 +21,19 @@ export class BatchComponent implements OnInit {
 			this.jsonSchemaService.getBatchById(params['id']).subscribe((data) => {
 				this.batch = data;
 			});
+		});
+	}
+
+	download() {
+		this.jsonSchemaService.getJsonSchemaByBatchId(this.batch._id).subscribe((data) => {
+			if(data && data.length > 0){
+				const jsonSchema = data[0].jsonSchema;
+				const fileName = `${this.batch.collectionName}.json`;
+				const fileToSave = new Blob([JSON.stringify(jsonSchema)], {
+					type: 'application/json'
+    			});
+				FileSaver.saveAs(fileToSave, fileName);		
+			}
 		});
 	}
 
